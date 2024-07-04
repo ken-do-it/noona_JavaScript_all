86ac283bc431ac981202d73a82cc81bc36b52187
kijin lee <lqizhen@naver.com>
2024-07-05 00:38:09 +1000

    mission complete final

--- todo_app/script.js
+++ todo_app/script.js
@@ -1,10 +1,34 @@
 let taskInput = document.getElementById("task_input");
 let addButton = document.getElementById("add_button");
+let tabs = document.querySelectorAll(".task_tabs div");
+let horizontalUnderLine = document.getElementById("under_line")
 let taskList = []
+let mode = 'all';
+let filterList = [];
+let message = document.getElementById("message");
+
+
 addButton.addEventListener("click", addTask)
 
+taskInput.addEventListener("focus", clearMessage);  
+
+
+
+for (let i = 1; i < tabs.length; i++){
+    tabs[i].addEventListener("click", function(event){
+        filter(event)
+        horizontalIndicator(event);
+    });
+}
+
+
 function addTask() {
-    // let taskContent = taskInput.value
+    let taskContent = taskInput.value
+    if (taskContent === "") {
+        showMessage("할 일을 입력해주세요");
+        return;
+    }
+
     let task = {
         id: randomIDGenerate(),
         taskContent: taskInput.value, 
@@ -13,26 +37,50 @@ function addTask() {
     taskList.push(task)
     console.log(taskList)
     render();
+    taskInput.value = "";
+    clearMessage(); // Clear message after adding task
+}
+
+taskInput.addEventListener("keypress", handleEnterPress);
+
+function handleEnterPress(event) {
+  if (event.key === "Enter") {
+    addTask();
+  }
+}
+
+
+function clearMessage() {
+    showMessage("");  // Clear the message when input gets focus
 }
 
 function render() {
-    let resultHTML = ``;
-    for (let i = 0; i < taskList.length; i++) {
+    let result = "";
+    let list = [];
+    if (mode === "all") {
+        list = taskList;
+    } else if (mode === "ongoing" || mode === "done") {
+        list = filterList;
+    }
 
-        if (taskList[i].isComplete == true) {
-            resultHTML +=`<div class="task">
-                    <div class = "task_done">${taskList[i].taskContent}</div> 
-                    <div> 
-                        <i class="fa-solid fa-arrow-rotate-left icon_size" onclick = "toggleComplete('${taskList[i].id}')"></i>
-                        <i class="fa-solid fa-trash-can icon_size2" onclick = "deleteTask('${taskList[i].id}')"></i>
+
+    let resultHTML = ``;
+    for (let i = 0; i < list.length; i++) {
+
+        if (list[i].isComplete == true) {
+            resultHTML +=`<div class="task task_done">
+                    <div class  = "task_item">${list[i].taskContent}</div> 
+                    <div class = "icon_con"> 
+                        <button><i class="fa-solid fa-arrow-rotate-left icon_size" onclick = "toggleComplete('${list[i].id}')"></i></button>
+                        <button><i class="fa-solid fa-trash-can icon_size2" onclick = "deleteTask('${list[i].id}')"></i></button>
                     </div>
                 </div>`;
         } else { 
             resultHTML += `<div class="task">
-            <div>${taskList[i].taskContent}</div> 
+            <div class  = "task_item">${list[i].taskContent}</div> 
             <div class = "icon_con">
-                <i class="fa-solid fa-circle-check icon_size" onclick = "toggleComplete('${taskList[i].id}')"></i>
-                <i class="fa-solid fa-trash-can icon_size2" onclick = "deleteTask('${taskList[i].id}')"></i>
+                <button><i class="fa-solid fa-circle-check icon_size" onclick = "toggleComplete('${list[i].id}')"></i></button>
+                <button><i class="fa-solid fa-trash-can icon_size2" onclick = "deleteTask('${list[i].id}')"></i></button>
             </div>
         </div>`;}
         
@@ -50,8 +98,8 @@ function toggleComplete(id) {
             break;
         }
     }
-    render();
-    console.log(taskList);
+    // render();
+    filter(); // 진행중에서 삭제시 즉각 사라지기
 }
 
 
@@ -62,6 +110,48 @@ function deleteTask(id) {
             break;
         }
     }
+    // render();
+    filter()
+}
+
+
+function filter(event) {
+
+    if (event) {
+        mode = event.target.id;
+        tabs.forEach(menu =>menu.addEventListener("click", (event) => horizontalIndicator(event)));
+        function horizontalIndicator(event) {
+            horizontalUnderLine.style.left = event.currentTarget.offsetLeft + "px";
+            horizontalUnderLine.style.width = event.currentTarget.offsetWidth + "px";
+            horizontalUnderLine.style.top = event.currentTarget.offsetTop + event.currentTarget.offsetHeight +"px";
+        }
+        
+    }
+   
+    filterList = [];
+    
+
+    if (mode === "all") {
+        render();
+    } 
+    
+     if (mode === "ongoing") {
+        for (let i = 0; i < taskList.length; i++) {
+            if (taskList[i].isComplete === false){
+                filterList.push(taskList[i])
+            }
+        }
+        render();
+    } else if (mode === "done") {
+        //끝나는 케이스 
+        //task.isComplete = true
+        for (let i = 0; i<taskList.length; i++) {
+            if (taskList[i].isComplete === true) {
+                filterList.push(taskList[i])
+            }
+        }
+        render();
+    }
     render();
 }
 
@@ -69,6 +159,10 @@ function randomIDGenerate() {
     return '_' + Math.random().toString(36).substr(2, 9);
 }
 
+
+function showMessage(msg) {
+    message.innerText = msg;
+}
 // console.log(document.getElementById("test").innerHTML)   // <span>hi</span>  test class 안에 안에 있는 태그까지 가지고 옴
 // console.log(document.getElementById("test").textContent) //   hi  test class 안에 있는 text 만 가지고 옴
 
