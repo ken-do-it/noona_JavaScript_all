9070b46d093336734095dac9c57e5cc1cefc1954
kijin lee <lqizhen@naver.com>
2024-07-08 19:53:31 +1000

    API change to noona API

--- API/main.js
+++ API/main.js
@@ -2,8 +2,8 @@ const API_KEY = `01402902efde4961a35ed0f1df9f81a3`;
 let news = []
 
 const getLatesNews = async () => {
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
-    // const url = new URL (`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`)
+    // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    const url = new URL (`https://noonaapi.netlify.app//top-headlines`)
     const response = await fetch(url);
     const data = await response.json()
      news = data.articles
